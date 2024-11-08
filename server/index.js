ab228577f6a77ea2e0c66ffdf16676680caabf7d
Edwin vargas <edwin548vargas@gmail.com>
2024-11-07 22:24:33 -0600

    Added server functions

---
+++ server/index.js
@@ -0,0 +1,21 @@
+import { format, transports } from "winston";
+import { createServer } from "node:http";
+import { createApp, logger } from "./src/index.js";
+
+const httpServer = createServer();
+
+await createApp(httpServer, {
+  postgres: {
+    user: "postgres",
+    password: "changeit",
+  },
+  sessionSecrets: ["changeit"],
+  cors: {
+    origin: ["http://localhost:5173"],
+    credentials: true,
+  },
+});
+
+httpServer.listen(3000, () => {
+  logger.info("server listening at http://localhost:3000");
+});
