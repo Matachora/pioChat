f55a7c6d89c80aca4a9e4d5406d119789129c7c6
Edwin vargas <edwin548vargas@gmail.com>
2024-11-10 13:28:38 -0600

    añadido angular client v 18

---
+++ client/src/app/app.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { NavBarComponent } from './modulos/nav-bar/nav-bar.component';
+import { NavBar2Component } from './modulos/nav-bar-2/nav-bar-2.component';
+
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  imports: [RouterOutlet, NavBarComponent, NavBar2Component],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.css'
+})
+export class AppComponent {
+
+}
