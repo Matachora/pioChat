1871d310a1e442499336c29eeb0eb8246fb5141d
BRDiego1 <nixonzapata507@gmail.com>
2024-11-13 07:34:02 -0600

    registro y login

---
+++ client/src/app/modulos/registro/registro.component.ts
@@ -0,0 +1,44 @@
+import {ChangeDetectionStrategy, Component, signal} from '@angular/core';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatIconModule} from '@angular/material/icon';
+import {MatInputModule} from '@angular/material/input';
+import {takeUntilDestroyed} from '@angular/core/rxjs-interop';
+import {FormControl, FormsModule, ReactiveFormsModule, Validators} from '@angular/forms';
+import {merge} from 'rxjs';
+import {MatButtonModule} from '@angular/material/button';
+
+@Component({
+  selector: 'app-registro',
+  standalone: true,
+  imports: [MatFormFieldModule, MatInputModule, MatIconModule, FormsModule, ReactiveFormsModule,MatButtonModule,],
+  templateUrl: './registro.component.html',
+  styleUrl: './registro.component.css'
+})
+export class RegistroComponent {
+  readonly email = new FormControl('', [Validators.required, Validators.email]);
+
+  errorMessage = signal('');
+
+  constructor() {
+    merge(this.email.statusChanges, this.email.valueChanges)
+      .pipe(takeUntilDestroyed())
+      .subscribe(() => this.updateErrorMessage());
+  }
+
+  updateErrorMessage() {
+    if (this.email.hasError('required')) {
+      this.errorMessage.set('You must enter a value');
+    } else if (this.email.hasError('email')) {
+      this.errorMessage.set('Not a valid email');
+    } else {
+      this.errorMessage.set('');
+    }
+  }
+
+  hide = signal(true);
+  clickEvent(event: MouseEvent) {
+    this.hide.set(!this.hide());
+    event.stopPropagation();
+  }
+
+}
